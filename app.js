e6b03c0497db3218ae64ffae0263fe04faef04d8
Yan-Exe <yanpatrick199@gmail.com>
2024-07-21 15:15:54 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,63 @@
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const scrollReveal = ScrollReveal({
+        origin: 'auto',
+        distance: '10px',
+        duration: 700,
+        reset: true
+        
+    });
+
+    scrollReveal.reveal('.efeite-reveal', {
+        interval: 100
+    });
+});
+
+function toggleTexto() {
+    var textoOculto = document.querySelector('.texto__oculto');
+    var botao = document.querySelector('.ocultar__texto');
+    var fotoSobreMim = document.querySelector('.sobre-mim__foto');
+
+    if (textoOculto.style.display === "none" || textoOculto.style.display === "") {
+        textoOculto.style.display = "block";
+        botao.textContent = "Ver menos";
+
+        if (window.innerWidth < 600) {
+            fotoSobreMim.style.display = "none";
+        }
+    } else {
+        textoOculto.style.display = "none";
+        botao.textContent = "Ver mais";
+
+        if (window.innerWidth < 600) {
+            fotoSobreMim.style.display = "block";
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('.habilidades__cards').forEach(function(card) {
+        card.style.display = 'block';
+    });
+
+    const swiper = new Swiper('.swiper', {
+        spaceBetween: 10,
+        slidesPerView: 1,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        scrollbar: {
+            el: '.swiper-scrollbar',
+        },
+    });
+});
+
+
+
